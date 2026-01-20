2e7f6b8363c3cb026fe2882e82650990bd79d0a3
FatihTheDev <130474375+FatihTheDev@users.noreply.github.com>
2026-01-20 13:39:03 +0000

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -889,7 +889,7 @@ install_base() {
     dialog --infobox "Installing base system and essential packages..." 5 60
     
     # Base packages
-    pacstrap "$MOUNT_POINT" base base-devel "${KERNEL_PACKAGES[@]}" linux-firmware \
+    pacstrap "$MOUNT_POINT" base base-devel wget "${KERNEL_PACKAGES[@]}" linux-firmware \
              btrfs-progs networkmanager dialog reflector nano sudo \
              grub efibootmgr dosfstools os-prober mtools
     
